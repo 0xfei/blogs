f0d93f9ba370711afe8f1869ebb2bba3bf8de702
0xfei <hhxlovety@gmail.com>
2016-10-18 14:50:36 +0800

    add docker container name

--- dockerun.sh
+++ dockerun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-sudo docker run -it -d -p 80:80 -v `pwd`/public:/var/www/public -v `pwd`/nginx_conf:/etc/nginx/conf.d -v `pwd`/nginx_log:/var/log/nginx nginx /bin/sh -c "/usr/sbin/nginx && while true; do sleep 100; done"
+sudo docker run -it -d -p 80:80 --name web_site -v `pwd`/public:/var/www/public -v `pwd`/nginx_conf:/etc/nginx/conf.d -v `pwd`/nginx_log:/var/log/nginx nginx /bin/sh -c "/usr/sbin/nginx && while true; do sleep 100; done"
