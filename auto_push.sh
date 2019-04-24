6a210acfe5dafefd348c8cf495245d412780614b
liuyufei.hhx <liuyufei.hhx@bytedance.com>
2019-04-24 10:52:25 +0800

    use new theme

--- auto_push.sh
+++ auto_push.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Push public/* to 0x01f.com !"
+echo "Push public/* to 0x01f.cn !"
 
 echo "Start pushing..."
 
 filename="1.tar.gz"
 origfiles="public/*"
-remote="root@0x01f.com:/var/www/"
+remote="root@0x01f.cn:/var/www/"
 sshdir="/var/www/"
 # Do not execute `rm -rf public`
 cmd="cd ${sshdir}; tar -xzf $filename -C $sshdir; rm $filename"
 
 tar -czf $filename $origfiles
 scp $filename $remote
-ssh root@0x01f.com $cmd
-
+ssh root@0x01f.cn $cmd
 echo "Remove $filename."
 rm $filename
 
